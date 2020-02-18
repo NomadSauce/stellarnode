429242920223966a0dbbfc0b237a28b37f26efe6
NomadSauce <mgarsteck@protonmail.com>
2020-02-18 01:19:05 -0800

    Adding routes and setup mongo

--- server.js
+++ server.js
@@ -1,8 +1,35 @@
+if (process.env.NODE_ENV !== 'production') {
+    require('dotenv').config()
+}
+
 const express = require('express');
 const app = express();
+const expressLayouts = require('express-ejs-layouts');
 const bcrypt = require('bcrypt');
 
+const indexRouter = require('./routes/index')
+
 app.use(express.json());
+
+app.set('view engine', 'ejs');
+app.set('views', __dirname + '/views');
+app.set('layout', 'layouts/layout');
+app.use(expressLayouts);
+app.use(express.static('public'));
+
+const mongoose = require('mongoose');
+mongoose.connect(process.env.DATABASE_URL, {
+    useNewUrlParser: true });
+const db = mongoose.connection;
+db.on('error', error => console.log(error));
+db.once('open', () => console.log('Connected to MongoDB'));
+
+app.use('/', indexRouter);
+
+
+
+// Stuff for users
+
 
 const users = [];
 
@@ -30,10 +57,11 @@ app.post('/users/login', async (req, res) => {
         if(await bcrypt.compare(req.body.password, user.password)) {
             res.send('Success')
         } else {
-            res.send('Not Allowed')
+            res.send('Not Allowed ')
         }
     } catch (e) {
         res.status(500).send('error')
     }
 });
-app.listen(5000);
+
+app.listen(process.env.PORT || 5000);
